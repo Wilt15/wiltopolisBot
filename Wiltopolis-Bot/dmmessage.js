84489fcdb0db2e1f717ce0733f9fb0f6f57c9b44
Wilt15 <46854840+Wilt15@users.noreply.github.com>
2019-12-16 14:27:13 -0600

    Update dmmessage.js

--- Wiltopolis-Bot/dmmessage.js
+++ Wiltopolis-Bot/dmmessage.js
@@ -27,7 +27,7 @@ module.exports = async (bot, message) => {
         .setColor("GREEN");
 
         message.author.send(dmEmbed);
-        channel.send(mEmbed);
+        channel.send('@everyone', mEmbed);
 
       } else {
         const mEmbed = new Discord.MessageEmbed()
@@ -40,7 +40,7 @@ module.exports = async (bot, message) => {
         if (message.attachments.array().length > 1) mEmbed.attachFiles(message.attachments.map(x => x.url))
         else if (message.attachments.array().length == 1) mEmbed.setImage(message.attachments.first().url)
 
-        channel.send('@everyone', mEmbed);
+        channel.send(mEmbed);
       }
     }
   }
