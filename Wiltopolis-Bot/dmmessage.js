8c48060f1ac7d28823ba985098bc4f62f0d28e39
Wilt15 <46854840+Wilt15@users.noreply.github.com>
2019-12-16 14:45:01 -0600

    Update dmmessage.js

--- Wiltopolis-Bot/dmmessage.js
+++ Wiltopolis-Bot/dmmessage.js
@@ -39,8 +39,8 @@ module.exports = async (bot, message) => {
 
         if (message.attachments.array().length > 1) mEmbed.attachFiles(message.attachments.map(x => x.url))
         else if (message.attachments.array().length == 1) mEmbed.setImage(message.attachments.first().url)
-
-        channel.send('@everyone', mEmbed);
+        
+        channel.send('w!userinfo ${message.author.username}', @everyone', mEmbed);
       }
     }
   }
